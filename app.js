4b8954dbe763a8588194491af61e562a7f324ee2
Marcelo Peres Toi <marcelo_toi@hotmail.com>
2017-11-24 03:19:32 -0200

    Adicionado documentacao swagger

--- app.js
+++ app.js
@@ -5,11 +5,13 @@
  */
 const express = require('express');
 const session = require('express-session');
-const request = require('supertest');
+var swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
+
 app = express();
 port = process.env.PORT || 3000;
 mongoose = require('mongoose'),
-User = require('./api/models/models'), //Importa os modelos de usuario
+User = require('./api/models/models'),
 bodyParser = require('body-parser');
 
 //Conexao do mongoose
@@ -20,6 +22,7 @@ mongoose.connect('mongodb://localhost/');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(session({secret:"hsadiuh323ujwqijsj1ijks", resave: false, saveUnitialized: true}));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const routes = require('./api/routes/routes'); //Importa as rotas
 routes(app); //Implementa as rotas no app
